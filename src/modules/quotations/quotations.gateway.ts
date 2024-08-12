82f4f7fc32958132aefd3d1096f7c045b5752223
Lucas Menezes <luckmenez@gmail.com>
2024-08-12 18:14:30 -0300

    fix: alterado endpoint para cors

--- src/modules/quotations/quotations.gateway.ts
+++ src/modules/quotations/quotations.gateway.ts
@@ -13,7 +13,7 @@ import { UsersService } from '../users/users.service';
 @UseGuards(WsAuthGuard)
 @WebSocketGateway({
   cors: {
-    origin: '*', // Permitir conexões de qualquer origem
+    origin: `${process.env.CORS_DOMAIN}`, // Permitir conexões de qualquer origem
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,

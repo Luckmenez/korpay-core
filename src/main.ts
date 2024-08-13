3ec869db962e58ab038ef08fdb13348fe28e3adc
Lucas Menezes <luckmenez@gmail.com>
2024-08-13 00:32:33 -0300

    fix: add cors para websocket aws

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `${process.env.CORS_DOMAIN}`,
+    origin: `*`,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());

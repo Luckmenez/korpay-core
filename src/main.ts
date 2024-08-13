39aec083f90071af4fe80dfc8fa3c801a38de5ec
Lucas Menezes <luckmenez@gmail.com>
2024-08-13 00:40:29 -0300

    fix: add cors para websocket aws

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `*`,
+    origin: 'https://korpay-6wm9zxds7-luckmenezs-projects.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());

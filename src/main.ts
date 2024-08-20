de73aa9dafd88f230520977c88e4b03347d161c1
Lucas Menezes <luckmenez@gmail.com>
2024-08-19 23:20:15 -0300

    feat: adicionado /api como rota de prefixo no backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.enableCors({
     origin: [process.env.CORS_DOMAIN, 'http://localhost:5173'],
     credentials: true,

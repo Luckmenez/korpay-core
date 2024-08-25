8d30049030e495208268cdca1fb72a66b80c640e
Lucas Menezes <luckmenez@gmail.com>
2024-08-25 17:25:19 -0300

    feat: adicionado novos valores para buy-crypto-dto

--- src/modules/quotations/dto/buy-crypto-dto.ts
+++ src/modules/quotations/dto/buy-crypto-dto.ts
@@ -1,13 +1,25 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class BuyCryptoDto {
+  @IsNumber()
+  @IsNotEmpty()
+  actual_spread: number;
+
   @IsString()
   @IsNotEmpty()
   quote_id: string;
 
+  @IsNumber()
+  @IsNotEmpty()
+  user_id: number;
+
   @IsString()
   @IsNotEmpty()
   amount: string;
+
+  @IsString()
+  @IsNotEmpty()
+  price: string;
 
   @IsString()
   @IsNotEmpty()

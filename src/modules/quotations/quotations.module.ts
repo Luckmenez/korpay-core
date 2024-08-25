ea56bdfcf2384bd1001958c8c3a80963eed4a4ae
Lucas Menezes <luckmenez@gmail.com>
2024-08-25 17:25:52 -0300

    feat: adicionado prismaService no quotation.service.ts

--- src/modules/quotations/quotations.module.ts
+++ src/modules/quotations/quotations.module.ts
@@ -5,10 +5,11 @@ import { WsAuthGuard } from './quotations.guard';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { QuotationsController } from './quotations.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  providers: [QuotationService, QuotationsGateway, WsAuthGuard],
+  providers: [QuotationService, QuotationsGateway, WsAuthGuard, PrismaService],
   controllers: [QuotationsController],
 })
 export class QuotationsModule {}

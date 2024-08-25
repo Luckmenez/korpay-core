b55e0b595025cfe0fdd63736508a4e8436a9b3f0
Lucas Menezes <luckmenez@gmail.com>
2024-08-25 17:20:25 -0300

    feat: criado interface para retorno da capitual

---
+++ src/@types/apiBuyReposponse.ts
@@ -0,0 +1,10 @@
+export interface BuyCryptoResponse {
+  data: {
+    order_id: 'f278d223-5999-4426-a95b-6cb5a06b3dc7';
+    crypto_value: '1088.8527';
+    settlement: 'd0';
+    settlement_date: '2023-12-05';
+    fiat_value: '5555.00';
+    currency_exchange: 'USDT';
+  };
+}

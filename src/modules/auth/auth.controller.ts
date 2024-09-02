171df8e4b6dd76da8b76ae87601af7330021de33
Lucas Menezes <luckmenez@gmail.com>
2024-09-01 21:39:58 -0300

    feat: adicionado serviços de reset e troca de senha

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthService } from './auth.service';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
+import { AuthResetPasswordDTO } from './dto/auth-reset.dto';
+import { AuthChangePasswordDTO } from './dto/auth-change.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +17,19 @@ export class AuthController {
   @Post('register')
   async register(@Body() authRegisterDTO: AuthRegisterDTO) {
     return await this.authService.register(authRegisterDTO);
+  }
+
+  @Post('reset-password')
+  async resetPassword(@Body() body: AuthResetPasswordDTO) {
+    return await this.authService.resetPassword(body.email);
+  }
+
+  @Post('change-password')
+  async changePassword(@Body() body: AuthChangePasswordDTO) {
+    return await this.authService.changePassword({
+      email: body.email,
+      password: body.password,
+      newPassword: body.newPassword,
+    });
   }
 }

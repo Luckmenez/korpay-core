43a62f6c31e20e06641176ec58f2bd79f3dfa7f2
Lucas Menezes <luckmenez@gmail.com>
2024-11-04 18:35:29 -0300

    feat: adicionado endpoint de updateStatus

--- src/modules/quotations/quotations.controller.ts
+++ src/modules/quotations/quotations.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { QuotationService } from './quotations.service';
 import { BuyCryptoDto } from './dto/buy-crypto-dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
 @Controller('quotation')
 export class QuotationsController {
@@ -9,5 +10,10 @@ export class QuotationsController {
   @Post('buy-crypto')
   async buyCrypto(@Body() body: BuyCryptoDto) {
     return await this.quotationsService.buyCrypto(body);
+  }
+
+  @Put('update-status')
+  async updateStatus(@Body() body: UpdateStatusDto) {
+    return await this.quotationsService.updateStatus(body);
   }
 }
